389086753ded88292627f622d081c01c5138e867
Min <als904204@naver.com>
2023-12-18 21:32:29 +0900

    spring init

---
+++ src/main/java/com/example/webduck/WebDuckApplication.java
@@ -0,0 +1,13 @@
+package com.example.webduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDuckApplication.class, args);
+    }
+
+}
