fc381034888d59d01bcab3832a928650a734972a
Min <als904204@naver.com>
2023-12-25 21:19:54 +0900

    cleanup:Member GUEST Role 삭제

--- src/main/java/com/example/webduck/Member/entity/Role.java
+++ src/main/java/com/example/webduck/Member/entity/Role.java
@@ -11,7 +11,6 @@ public enum Role {
 
     ADMIN("ROLE_ADMIN","관리자"),
     MANAGER("ROLE_MANAGER","매니저"),
-    GUEST("ROLE_GUEST","손님"),
     USER("ROLE_USER","일반 사용자");
 
     private final String key;
