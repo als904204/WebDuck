04720535526618227eca4a7fcee68945f53b2325
Min <als904204@naver.com>
2023-12-28 15:52:11 +0900

    feat:파일 업로드 인터페이스

---
+++ src/main/java/com/example/webduck/admin/service/FileStore.java
@@ -0,0 +1,10 @@
+package com.example.webduck.admin.service;
+
+import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStore {
+
+    String upload(MultipartFile file) throws IOException;
+
+}
