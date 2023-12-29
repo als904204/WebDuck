078e872c9c1a3ee9eb417376227d05274db22111
Min <als904204@naver.com>
2023-12-29 21:33:33 +0900

    feat:OAuth 예외 코드 추가

--- src/main/java/com/example/webduck/global/exception/exceptionCode/ValidationExceptionCode.java
+++ src/main/java/com/example/webduck/global/exception/exceptionCode/ValidationExceptionCode.java
@@ -8,6 +8,7 @@ public enum ValidationExceptionCode implements ExceptionCode {
 
     INVALID_LOCATION(400, "Invalid location value"),
     INVALID_STATE(400, "Invalid state value"),
+    INVALID_OAUTH_TYPE(400, "Invalid oauth type"),
 
     // 지원하지 않는 파일 확장자를 업로드 했을 때
     INVALID_FILE_TYPE(415, "Invalid file type");
