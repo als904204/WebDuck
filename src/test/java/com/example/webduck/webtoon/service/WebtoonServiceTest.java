07dff5d0b5c6fdcf748bf89a06cf295d639c75dc
Min <als904204@naver.com>
2023-12-31 21:13:09 +0900

    test:요일별 웹툰 조회 service 테스트 코드 작성 #3

--- src/test/java/com/example/webduck/webtoon/service/WebtoonServiceTest.java
+++ src/test/java/com/example/webduck/webtoon/service/WebtoonServiceTest.java
@@ -12,14 +12,11 @@ import com.example.webduck.webtoon.entity.Webtoon;
 import com.example.webduck.webtoon.repository.WebtoonRepository;
 import java.util.List;
 import java.util.Optional;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -80,10 +77,27 @@ class WebtoonServiceTest {
         when(webtoonRepository.findAll()).thenReturn(webtoons);
 
         List<WebtoonRequest> result = webtoonService.findWebtoonList();
-        Assertions.assertThat(result).isNotNull();
-        Assertions.assertThat(result).hasSize(4);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(4);
         verify(webtoonRepository, times(1)).findAll();
     }
 
 
+    @DisplayName("요일별 웹툰 목록 조회")
+    @Test
+    void findWebtoonsByPublicDay() {
+        List<Webtoon> webtoons = List.of(
+            Webtoon.builder().title("Webtoon 3").summary("Summary 3").imagePath("Path 3")
+                .publishDay(PublishDay.SUNDAY).originalImageName("Image3.png").build(),
+            Webtoon.builder().title("Webtoon 4").summary("Summary 4").imagePath("Path 4")
+                .publishDay(PublishDay.SUNDAY).originalImageName("Image4.png").build()
+        );
+
+        when(webtoonRepository.findWebtoonByPublishDay(PublishDay.SUNDAY)).thenReturn(webtoons);
+
+        List<WebtoonRequest> webtoonsByPublishDay = webtoonService.findWebtoonByPublishDay(PublishDay.SUNDAY);
+
+        assertThat(webtoonsByPublishDay).isNotNull();
+        assertThat(webtoonsByPublishDay).hasSize(2);
+    }
 }
