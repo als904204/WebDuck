66601526bfcc3fa6230e2c98844ee55d5fe9b82e
Min <als904204@naver.com>
2024-01-01 18:11:10 +0900

    feat:웹툰 필드에 플랫폼 추가 #3

--- src/main/java/com/example/webduck/webtoon/entity/Webtoon.java
+++ src/main/java/com/example/webduck/webtoon/entity/Webtoon.java
@@ -7,37 +7,72 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
 @Entity
 public class Webtoon{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     // 제목
     private String title;
+
     // 줄거리
     private String summary;
+
     // 파일의 원본 이름을 저장하면 나중에 관리자가 해당 파일을 식별하고 검색하는 용이
     private String originalImageName;
+
     // 썸네일 이미지 경로
     private String imagePath;
+
     // 웹툰 요일
     @Enumerated(EnumType.STRING)
     private PublishDay publishDay;
 
+    // 웹툰 플랫폼
+    @Enumerated(EnumType.STRING)
+    private Platform platform;
+
+    protected Webtoon() {}
+
     @Builder
-    public Webtoon(String title, String summary,String originalImageName, String imagePath,PublishDay publishDay) {
+    public Webtoon(String title, String summary, String originalImageName, String imagePath,
+        PublishDay publishDay, Platform platform) {
         this.title = title;
         this.summary = summary;
         this.originalImageName = originalImageName;
         this.imagePath = imagePath;
         this.publishDay = publishDay;
+        this.platform = platform;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getOriginalImageName() {
+        return originalImageName;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public PublishDay getPublishDay() {
+        return publishDay;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
 }
