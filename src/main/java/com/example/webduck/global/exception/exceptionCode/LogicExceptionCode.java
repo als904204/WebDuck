3efa8f87a6a4af8eaa53aa8af63998d76f431f17
Min <als904204@naver.com>
2024-01-03 17:07:03 +0900

    feat:Webtoon not found 예외 추가

--- src/main/java/com/example/webduck/global/exception/exceptionCode/LogicExceptionCode.java
+++ src/main/java/com/example/webduck/global/exception/exceptionCode/LogicExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum LogicExceptionCode implements ExceptionCode {
-    BAD_REQUEST(400, "Bad Request");
+    BAD_REQUEST(400, "Bad Request"),
+    WEBTOON_NOT_FOUND(404, "Webtoon not found");
 
     @Getter
     private int status;
