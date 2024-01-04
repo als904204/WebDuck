a1e08da118224298ec53460c412cdce17bce062a
Min <als904204@naver.com>
2024-01-04 17:05:24 +0900

    feat:장르별 웹툰 조회 jpa #8

--- src/main/java/com/example/webduck/webtoon/repository/WebtoonRepository.java
+++ src/main/java/com/example/webduck/webtoon/repository/WebtoonRepository.java
@@ -5,6 +5,8 @@ import com.example.webduck.webtoon.entity.PublishDay;
 import com.example.webduck.webtoon.entity.Webtoon;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,5 +16,13 @@ public interface WebtoonRepository extends JpaRepository<Webtoon, Long> {
 
     List<Webtoon> findWebtoonByPlatform(Platform platform);
 
+    // 장르에 해당하는 웹툰 찾기
+    // TODO : 페이징 적용된다면 QueryDSL 로 변경해야 함
+    @Query("SELECT w "
+        + "FROM Webtoon w "
+        + "INNER JOIN WebtoonGenre wg ON wg.webtoon.id = w.id "
+        + "INNER JOIN Genre g ON wg.genre.id = g.id "
+        + "WHERE g.type = :type")
+    List<Webtoon> findByGenreType(@Param("type") String type);
 
 }
