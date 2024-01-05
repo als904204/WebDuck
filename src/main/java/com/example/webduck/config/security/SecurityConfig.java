e1df76d5c46ea2c28d6c0db4bec1a21b2e601974
Min <als904204@naver.com>
2024-01-05 22:56:37 +0900

    fix:시큐리티 이미지 권한 모두 허용

--- src/main/java/com/example/webduck/config/security/SecurityConfig.java
+++ src/main/java/com/example/webduck/config/security/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
         "/img/**",
         "/js/**",
         "/auth/login",
-        "/images/**"
+        "/images/**",
+        "/publish/**"
     };
 
     private static final String[] WHITE_API_LIST_URL = {
