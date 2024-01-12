bd0d8decd8ab365b506ea38012cbc03dd9bf1ab3
Min <als904204@naver.com>
2024-01-12 17:04:53 +0900

    feat:add white api list

--- src/main/java/com/example/webduck/config/security/SecurityConfig.java
+++ src/main/java/com/example/webduck/config/security/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.example.webduck.config.security;
 
 import static com.example.webduck.member.entity.Role.ADMIN;
 import static com.example.webduck.member.entity.Role.MANAGER;
+import static com.example.webduck.member.entity.Role.USER;
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 import com.example.webduck.config.security.oauth.handler.OAuth2LoginFailureHandler;
 import com.example.webduck.config.security.oauth.handler.OAuth2LoginSuccessHandler;
 import com.example.webduck.config.security.oauth.service.CustomOAuth2UserService;
+import com.example.webduck.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,8 +44,14 @@ public class SecurityConfig {
 
     private static final String[] WHITE_GET_API_LIST_URL = {
         "/api/v1/webtoon/**",
-        "/api/v1/genre/**"
+        "/api/v1/genre/**",
+        "/api/v1/review/**"
     };
+
+    private static final String[] WHITE_POST_API_LIST_URL = {
+        "/api/v1/review/**"
+    };
+
 
     private static final String[] ADMIN_URL = {
         "/api/v1/admin/**",
@@ -58,6 +67,8 @@ public class SecurityConfig {
                     .permitAll()
                     .requestMatchers(GET, WHITE_GET_API_LIST_URL)
                     .permitAll()
+                    .requestMatchers(POST, WHITE_POST_API_LIST_URL)
+                    .hasAnyRole(ADMIN.name(),USER.name(),MANAGER.name())
                     .requestMatchers(ADMIN_URL)
                     .hasRole(ADMIN.name())
 
