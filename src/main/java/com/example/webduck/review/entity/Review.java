8eb7ba111299c386611b97b40ad6cec2f75ff402
Min <als904204@naver.com>
2024-01-16 01:00:53 +0900

    feat:리뷰 평균점수 구하기 도메인 로직

--- src/main/java/com/example/webduck/review/entity/Review.java
+++ src/main/java/com/example/webduck/review/entity/Review.java
@@ -1,11 +1,14 @@
 package com.example.webduck.review.entity;
 
 import com.example.webduck.global.common.BaseTime;
+import com.example.webduck.global.exception.CustomException;
+import com.example.webduck.global.exception.exceptionCode.LogicExceptionCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.List;
 import lombok.Builder;
 
 
@@ -36,7 +39,6 @@ public class Review extends BaseTime {
     @Column(nullable = false)
     private Integer rating;
 
-
     // todo : 세션맴버에서 닉네임 추가하고 리뷰 생성할 때 닉네임으로 설정
     @Builder
     public Review(Long webtoonId, Long memberId,String reviewerNickname ,String content,Integer rating) {
@@ -49,6 +51,19 @@ public class Review extends BaseTime {
 
     protected Review() {}
 
+    // 리뷰점수 평균 구하기
+    public static Double calculateRatingAvg(List<Review> reviews) {
+        if (reviews == null || reviews.isEmpty()) {
+            throw new CustomException(LogicExceptionCode.WEBTOON_NOT_FOUND);
+        }
+        Integer sum = 0;
+        for (Review review : reviews) {
+            sum += review.getRating();
+        }
+        return (double) sum / reviews.size();
+    }
+
+
     public Long getId() {
         return id;
     }
@@ -58,15 +73,12 @@ public class Review extends BaseTime {
     public Long getMemberId() {
         return memberId;
     }
-
     public String getContent() {
         return content;
     }
-
     public String getReviewerNickname() {
         return reviewerNickname;
     }
-
     public Integer getRating() {
         return rating;
     }
