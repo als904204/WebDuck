c7e11fc22c6118906dd22d6fa4d21fda2b69f105
Min <als904204@naver.com>
2024-01-16 21:33:47 +0900

    feat:인기웹툰(리뷰개수순,리뷰점수순) queryDsl 동적 쿼리

--- src/main/java/com/example/webduck/webtoon/repository/WebtoonGenreCustom.java
+++ src/main/java/com/example/webduck/webtoon/repository/WebtoonGenreCustom.java
@@ -1,8 +1,13 @@
 package com.example.webduck.webtoon.repository;
 
 import com.example.webduck.webtoon.dto.WebtoonGenreResponse;
+import com.example.webduck.webtoon.dto.WebtoonSortCondition.WebtoonConditionResponse;
 import java.util.List;
 
 public interface WebtoonGenreCustom {
+
     List<WebtoonGenreResponse> findWebtoonsByGenres(List<String> genreNames);
+
+    List<WebtoonConditionResponse> findPopularWebtoonsByCondition(String condition);
+
 }
