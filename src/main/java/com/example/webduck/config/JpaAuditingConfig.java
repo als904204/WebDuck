b0dc6069c84cd5da0ce2bed71bf515ff41c309d0
Min <als904204@naver.com>
2024-01-20 16:06:29 +0900

    style:주석 수정

--- src/main/java/com/example/webduck/config/JpaAuditingConfig.java
+++ src/main/java/com/example/webduck/config/JpaAuditingConfig.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * EnableJpaAuditing 설정 클래스
- * Main 에서 따로 구성한 이유는 Controller 테스트코드(@WebMvcTest) 작성할 때
+ * Main -> Config 로 따로 구성한 이유는 Controller 테스트코드(@WebMvcTest) 작성할 때
  * @WebMvcTest가 전체 스프링 컨텍스트를 로드하지 않기 때문에 JPA 메타모델이 초기화되지 않아 오류 발생
  */
 @Configuration
