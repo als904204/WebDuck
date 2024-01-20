2bc3e6366655bce1ecf6784614ce21146351c07c
Min <als904204@naver.com>
2024-01-20 15:25:28 +0900

    refactor:JpaAuditing 설정 클래스,테스트 코드 버그로인한 별도 구성

---
+++ src/main/java/com/example/webduck/config/JpaAuditingConfig.java
@@ -0,0 +1,13 @@
+package com.example.webduck.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * EnableJpaAuditing 설정 클래스
+ * Main 에서 따로 구성한 이유는 Controller 테스트코드(@WebMvcTest) 작성할 때
+ * @WebMvcTest가 전체 스프링 컨텍스트를 로드하지 않기 때문에 JPA 메타모델이 초기화되지 않아 오류 발생
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {}
