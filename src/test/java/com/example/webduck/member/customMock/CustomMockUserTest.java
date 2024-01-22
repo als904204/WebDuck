12a23c26126d45915e4613bdab35f49846925971
Min <als904204@naver.com>
2024-01-22 16:44:06 +0900

    test:테스트 환경 Spring Security Session Member 생성

---
+++ src/test/java/com/example/webduck/member/customMock/CustomMockUserTest.java
@@ -0,0 +1,29 @@
+package com.example.webduck.member.customMock;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.example.webduck.config.security.oauth.entity.SessionMember;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomMockUserTest {
+
+    @DisplayName("커스텀 MockMember 동작 확인")
+    @Test
+    @WithMockCustomUser(email="testEmail@test.com")
+    public void testCreateReview() throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SessionMember sessionMember = (SessionMember) authentication.getPrincipal();
+
+        assertThat(sessionMember.getEmail()).isEqualTo("testEmail@test.com");
+
+    }
+
+}
