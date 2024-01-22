0636516f34879a59ba9134506ff4208330da5e18
Min <als904204@naver.com>
2024-01-22 16:50:34 +0900

    feat:리뷰 응답 record 생성

--- src/main/java/com/example/webduck/review/dto/ReviewResponse.java
+++ src/main/java/com/example/webduck/review/dto/ReviewResponse.java
@@ -1,6 +1,7 @@
 package com.example.webduck.review.dto;
 
 import com.example.webduck.review.entity.Review;
+import lombok.Getter;
 
 public class ReviewResponse {
 
@@ -32,6 +33,13 @@ public class ReviewResponse {
     public Integer getRating() {
         return rating;
     }
+
+    public record ReviewId(Long reviewId) {}
+
+    public record ReviewAvg(Double reviewAvg) { }
+    public record ReviewCount(int reviewCount) { }
+
+
 }
 
 
