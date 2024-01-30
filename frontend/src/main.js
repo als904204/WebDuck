b1bb5e277d81ff7e98f4f9c31a717a9a7d767d84
Min <als904204@naver.com>
2024-01-30 13:24:36 +0900

    feat:[FE]PrimeVue 적용

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,24 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// Router
 import { router } from './router/index.js'
 
+// PrimeVue
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button"
+import Menubar from 'primevue/menubar';
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
+
 const app = createApp(App)
-app.use(router)  // 라우터 사용
+
+// router
+app.use(router)
+
+// prime
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('Menubar', Menubar);
+
 app.mount('#app')
