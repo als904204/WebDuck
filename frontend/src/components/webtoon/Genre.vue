374c7ad22fbfd8b0ca23681b4a112b4c1bd9806a
Min <als904204@naver.com>
2024-01-30 13:59:33 +0900

    feat:[FE]장르,요일,카카오,네이버,로그인 페이지 라우터 설정

---
+++ frontend/src/components/webtoon/Genre.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Genre!!</h1>
+</template>
+
+<script setup>
+
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from "../components/Home.vue";
+import Genre from "../components/webtoon/Genre.vue";
+import Publish from "../components/webtoon/publish.vue";
+import Kakao from "../components/webtoon/Kakao.vue";
+import Naver from "../components/webtoon/Naver.vue";
+import Login from "../components/login/Login.vue";
 
 const routes = [
-  {path : '/',component:Home}
+  {path : '/',component:Home},
+  {path : '/genre', component: Genre},
+  {path : '/publish', component: Publish},
+  {path : '/kakao', component: Kakao},
+  {path : '/naver', component: Naver},
+  {path : '/login', component: Login},
 ]
 
 
