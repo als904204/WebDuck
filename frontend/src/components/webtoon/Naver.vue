77a4bf863f15f843613a2f21a2a160ea5f324e89
Min <als904204@naver.com>
2024-02-01 23:06:17 +0900

    feat:네이버 웹툰 API 호출

--- frontend/src/components/webtoon/Naver.vue
+++ frontend/src/components/webtoon/Naver.vue
@@ -1,7 +1,22 @@
 <template>
-  <h1>Naver!!</h1>
+<h1>NAVER</h1>
+  <ul v-if="webtoons.length > 0">
+    <li v-for="webtoon of webtoons" :key="webtoon.id">
+      {{webtoon.title}}
+    </li>
+  </ul>
 </template>
 
 <script setup>
+import {getWebtoonsByPlatform} from "../../service/WebtoonListFetchService.js";
+import {onMounted, ref} from "vue";
+
+
+const webtoons = ref([]);
+
+onMounted(async () => {
+  const data = await getWebtoonsByPlatform("NAVER");
+  webtoons.value = data;
+})
 
 </script>
