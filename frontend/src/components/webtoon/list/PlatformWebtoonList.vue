bdc649c346b558b40f4984736998e87126555f02
Min <als904204@naver.com>
2024-02-01 15:11:48 +0900

    refactor:[FE] 웹툰 이미지 css 반응형

--- frontend/src/components/webtoon/list/PlatformWebtoonList.vue
+++ frontend/src/components/webtoon/list/PlatformWebtoonList.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="webtoon-grid">
     <div class="webtoon-item" v-for="webtoon in webtoons" :key="webtoon.id">
-      <img class="webtoon-image" :src="webtoon.imagePath" :alt="webtoon.title">
-      <p class="webtoon-title">{{ webtoon.title }}</p>
+      <router-link :to="`/webtoon/${webtoon.id}`">
+        <img class="webtoon-image" :src="webtoon.imagePath" :alt="webtoon.title">
+      </router-link>
+        <p class="webtoon-title">{{ webtoon.title }}</p>
     </div>
   </div>
 </template>
@@ -40,14 +42,46 @@ watch(() => props.platform,async (requestPlatform) => {
   flex-direction: column;
   align-items: center;
 }
+.webtoon-item a {
+  text-decoration: none;
+  color: inherit;
+}
 
-.webtoon-image{
-  width:100%;
-  height: 100%;
+
+.webtoon-image {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
 }
 
 .webtoon-title{
   white-space: nowrap;
   max-width: 100%;
+}
+
+
+/* 화면 크기에 따라 열의 개수를 조정합니다 */
+@media (max-width: 1200px) { /* 데스크탑 크기 */
+  .webtoon-grid {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+@media (max-width: 992px) { /* 태블릿 크기 */
+  .webtoon-grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (max-width: 768px) { /* 중간 크기의 태블릿 */
+  .webtoon-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (max-width: 576px) { /* 모바일 크기 */
+  .webtoon-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 </style>
