a0a38b1bc3cc1b5f20ab653fddff15f585aff85d
Min <als904204@naver.com>
2024-02-02 15:27:17 +0900

    feat:[FE]장르 버튼 한글로 맵핑

--- frontend/src/components/webtoon/Genre.vue
+++ frontend/src/components/webtoon/Genre.vue
@@ -1,6 +1,53 @@
 <template>
-  <h1>장르!</h1>
+  <!-- 장르 종류-->
+  <div>
+    <div class="button-container">
+      <Button v-for="genre in genres" :key="genre.genreName" :label="genre.genreName" class="p-button" />
+    </div>
+    <FetchWebtoonList :genreNames="['ACTION', 'DRAMA']" serviceType="GENRES" />
+  </div>
+
 </template>
 
 <script setup>
+import {getGenres, getWebtoonsByGenres} from "../../service/WebtoonListFetchService.js";
+import {onMounted, ref} from "vue";
+import FetchWebtoonList from "./list/WebtoonList.vue";
+
+const genresToKorean = {
+  "ACTION": "액션",
+  "ADULT": "성인",
+  "DAILYLIFE": "일상",
+  "DRAMA": "드라마",
+  "FANTASY": "판타지",
+  "GAG": "개그",
+  "MARTIALARTS": "무협",
+  "ROMANCE": "로맨스",
+  "SPORTS": "스포츠",
+  "THRILLER": "스릴러"
+};
+
+const genres = ref([]);
+
+onMounted(async () => {
+  try {
+    const apiResponse = await getGenres();
+    genres.value = apiResponse.map(genre => ({
+      genreName: genresToKorean[genre.genreName] || genre.genreName
+    }));
+  } catch (error) {
+    console.error("error while request genres:", error);
+  }
+});
+
+
 </script>
+
+<style scoped>
+.button-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
+  justify-content: center;
+}
+</style>
