e46328530c42e6b6183683d18a43fcf8ea31440f
Min <als904204@naver.com>
2024-02-07 14:42:13 +0900

    feat:[FE]카카오,구글,로그인 버튼 클릭시 백엔드 로그인 URL 요청

--- frontend/src/components/login/Login.vue
+++ frontend/src/components/login/Login.vue
@@ -6,13 +6,13 @@
       </template>
       <template #content>
         <div class="button-container">
-          <a href="" class="btn-link">
+          <a :href="googleLoginUrl" class="btn-link">
             <img :src="google" class="login-icon" alt="google_login">
           </a>
-          <a href="" class="btn-link">
+          <a :href="naverLoginUrl" class="btn-link">
           <img :src="naver" class="login-icon" alt="naver_login">
           </a>
-          <a href="" class="btn-link">
+          <a :href="kakaoLoginUrl"class="btn-link">
           <img :src="kakao" class="login-icon" alt="kakao_login">
           </a>
         </div>
@@ -24,6 +24,15 @@
 import google from '../../assets/login/btn_google.svg';
 import naver from '../../assets/login/btn_naver.svg';
 import kakao from '../../assets/login/btn_kakao.svg';
+import { computed } from 'vue';
+
+
+// TODO env 로 변경
+const backendUrl = "http://localhost:8080"
+const googleLoginUrl = computed(() => `${backendUrl}/oauth2/authorization/google`);
+const naverLoginUrl = computed(() => `${backendUrl}/oauth2/authorization/naver`);
+const kakaoLoginUrl = computed(() => `${backendUrl}/oauth2/authorization/kakao`);
+
 
 
 </script>
