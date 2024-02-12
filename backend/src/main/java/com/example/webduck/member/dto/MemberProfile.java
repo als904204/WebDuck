5ee707a65ce2466c12d87b9fa9f36c77b84dec10
Min <als904204@naver.com>
2024-02-13 01:35:19 +0900

    refactor:[BE]회원 프로필 dto 리팩토링

--- backend/src/main/java/com/example/webduck/member/dto/MemberProfile.java
+++ backend/src/main/java/com/example/webduck/member/dto/MemberProfile.java
@@ -1,19 +1,33 @@
 package com.example.webduck.member.dto;
 
 import com.example.webduck.member.entity.Member;
-import com.example.webduck.member.entity.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class MemberProfile {
 
-    private final Long id;
-    private final String username;
-    private final Role role;
-
-    public MemberProfile(Member member) {
-        this.id = member.getId();
-        this.username = member.getUsername();
-        this.role = member.getRole();
+    @Getter
+    public static class ProfileResponse {
+        private final String username;
+        public ProfileResponse(Member member) {
+            this.username = member.getUsername();
+        }
     }
+
+    @Getter
+    public static class ProfileRequest{
+
+        @NotBlank(message = "닉네임은 빈칸이 될 수 없습니다")
+        @Size(min = 2,max = 16, message = "닉네임은 최대 16까지 가능합니다")
+        private String username;
+        public ProfileRequest(String username) {
+            this.username = username;
+        }
+        public ProfileRequest() {}
+    }
+
 }
