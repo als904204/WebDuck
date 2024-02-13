a793759290788c27b3746e825c67c712bf37108a
Min <als904204@naver.com>
2024-02-13 15:30:51 +0900

    feat:[BE]세션 유저 null 검증 로직

--- backend/src/main/java/com/example/webduck/config/security/oauth/resolver/LoginMemberArgumentResolver.java
+++ backend/src/main/java/com/example/webduck/config/security/oauth/resolver/LoginMemberArgumentResolver.java
@@ -2,9 +2,12 @@ package com.example.webduck.config.security.oauth.resolver;
 
 import com.example.webduck.config.security.oauth.dto.LoginMember;
 import com.example.webduck.config.security.oauth.entity.SessionMember;
+import com.example.webduck.global.exception.CustomException;
+import com.example.webduck.global.exception.exceptionCode.LogicExceptionCode;
 import jakarta.servlet.http.HttpSession;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -15,6 +18,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 /**
  * LoginMember 어노테이션에 사용자 정보 주입하는 Resolver
  */
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
@@ -34,6 +38,14 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
         @NonNull MethodParameter parameter,
         ModelAndViewContainer mavContainer,
         @NonNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        var sessionMember = httpSession.getAttribute("member");
+
+        if (sessionMember == null) {
+            log.error("Authenticated user not found in session");
+            throw new CustomException(LogicExceptionCode.MEMBER_NOT_FOUND);
+        }
+
         return httpSession.getAttribute("member");
     }
 }
