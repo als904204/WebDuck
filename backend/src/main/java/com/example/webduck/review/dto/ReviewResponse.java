bfcbe00aaa3d4c36abc76ec082d626fd2523eff1
Min <als904204@naver.com>
2024-02-14 15:06:18 +0900

    feat:[BE]리뷰 좋아요 API

--- backend/src/main/java/com/example/webduck/review/dto/ReviewResponse.java
+++ backend/src/main/java/com/example/webduck/review/dto/ReviewResponse.java
@@ -1,11 +1,15 @@
 package com.example.webduck.review.dto;
 
 
+import lombok.Getter;
+
 public class ReviewResponse {
 
     public record ReviewId(Long reviewId) {}
     public record ReviewAvg(Double rating) { }
     public record ReviewCount(int count) { }
+    public record ReviewLikesResponse(String message, int likesCount) { }
+
 
 
 }
--- backend/src/main/java/com/example/webduck/review/dto/SliceReviewResponse.java
+++ backend/src/main/java/com/example/webduck/review/dto/SliceReviewResponse.java
@@ -12,15 +12,17 @@ public class SliceReviewResponse {
     private final Long authorId;
     private final Integer rating;
     private final LocalDateTime createdAt;
+    private final int likesCount;
 
     public SliceReviewResponse(Long reviewId, String content, String reviewerNickname,
-        Long authorId, Integer rating, LocalDateTime createdAt) {
+        Long authorId, Integer rating, LocalDateTime createdAt,int likesCount) {
         this.reviewId = reviewId;
         this.content = content;
         this.reviewerNickname = reviewerNickname;
         this.authorId = authorId;
         this.rating = rating;
         this.createdAt = createdAt;
+        this.likesCount = likesCount;
     }
 
 }
