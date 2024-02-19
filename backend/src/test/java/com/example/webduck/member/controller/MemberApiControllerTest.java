da5cdc1305c673e12be9d66370505c74d44706b2
Min <als904204@naver.com>
2024-02-19 18:28:01 +0900

    test:profile -> test 변경

--- backend/src/test/java/com/example/webduck/member/controller/MemberApiControllerTest.java
+++ backend/src/test/java/com/example/webduck/member/controller/MemberApiControllerTest.java
@@ -19,8 +19,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @WebMvcTest(MemberApiController.class)
 class MemberApiControllerTest {
 
