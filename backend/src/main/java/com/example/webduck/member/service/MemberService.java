6a6de924ea3330474567f7190c33aff283798973
Min <als904204@naver.com>
2024-03-10 22:13:55 +0900

    refactor:[BE]가입/로그인 OAuth에서 MemberService로 변경

--- backend/src/main/java/com/example/webduck/member/service/MemberService.java
+++ backend/src/main/java/com/example/webduck/member/service/MemberService.java
@@ -3,19 +3,24 @@ package com.example.webduck.member.service;
 import com.example.webduck.global.security.oauth.entity.SessionMember;
 import com.example.webduck.global.exception.CustomException;
 import com.example.webduck.global.exception.exceptionCode.LogicExceptionCode;
+import com.example.webduck.global.security.oauth.entity.userInfo.OAuth2UserInfo;
 import com.example.webduck.member.domain.MemberProfile;
 import com.example.webduck.member.dto.MemberUpdate.ProfileRequest;
 import com.example.webduck.member.dto.MemberUpdate.ProfileResponse;
 import com.example.webduck.member.entity.Member;
-import com.example.webduck.member.repository.MemberRepository;
+import com.example.webduck.member.entity.Role;
+import com.example.webduck.member.service.port.MemberRepository;
 import com.example.webduck.review.entity.Review;
-import com.example.webduck.review.repository.ReviewRepository;
+import com.example.webduck.review.service.port.ReviewRepository;
+import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Builder
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -24,6 +29,28 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     private final ReviewRepository reviewRepository;
+
+    // TODO : 인터페이스 분리
+    private final NicknameGenerator nicknameGenerator;
+
+
+
+    @Transactional
+    public Member createOrUpdateMember(OAuth2UserInfo oAuth2UserInfo) {
+        // 기존 회원 검색 또는 신규 회원 생성
+        Member member = memberRepository.findByEmailAndSocialType(
+            oAuth2UserInfo.getEmail(),
+            oAuth2UserInfo.getSocialType())
+            .orElseGet(() -> {
+                // 신규 회원일 경우, 랜덤 닉네임으로 회원 생성
+                String randomNickname = nicknameGenerator.getRandomNickname();
+                return Member.from(oAuth2UserInfo, randomNickname);
+            });
+
+        // 최근 로그인 시간 업데이트 후 저장
+        member.updatePrevLoginAt(LocalDateTime.now());
+        return memberRepository.save(member);
+    }
 
 
     /**
@@ -34,14 +61,14 @@ public class MemberService {
      */
     @Transactional(readOnly = true)
     public MemberProfile getProfile(SessionMember sessionMember) {
-        Long sessionMemberId = sessionMember.getId();
+        Long memberId = sessionMember.getId();
 
         // TODO 세션 회원 들어오면 필터로 검증 서비스에선 Member로
-        Member member = memberRepository.findById(sessionMemberId)
+        Member member = memberRepository.findById(memberId)
             .orElseThrow(() -> new CustomException(
                 LogicExceptionCode.MEMBER_NOT_FOUND));
 
-        List<Review> memberReviews = reviewRepository.findByMemberId(sessionMemberId);
+        List<Review> memberReviews = reviewRepository.findByMemberId(memberId);
 
         return MemberProfile.from(member, memberReviews);
     }
