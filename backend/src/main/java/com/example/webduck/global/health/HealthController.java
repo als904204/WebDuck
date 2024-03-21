06480e3947e08e4a7a9ea5ee2e06f2543b0091c7
Min <als904204@naver.com>
2024-03-22 02:02:55 +0900

    feat:cicd 성공,확인용 api 생성

--- backend/src/main/java/com/example/webduck/global/health/HealthController.java
+++ backend/src/main/java/com/example/webduck/global/health/HealthController.java
@@ -1,6 +1,10 @@
 package com.example.webduck.global.health;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +17,10 @@ public class HealthController {
     private String version;
 
     @GetMapping
-    public String version() {
-        return String.format("version : %s", version);
+    public ResponseEntity<Map<String, String>> version() {
+        Map<String, String> map = new HashMap<>();
+        map.put("version", version);
+        return ResponseEntity.ok().body(map);
     }
 
-    @GetMapping("/health")
-    public String checkHealth() {
-        return "success";
-    }
 }
