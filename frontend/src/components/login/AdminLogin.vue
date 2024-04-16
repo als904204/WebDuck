759edd411fc9c1147b73b5dfdada87d0c146e880
Min <als904204@naver.com>
2024-04-16 18:06:01 +0900

    fix:[FE]관리자 로그인 적용 안되는 버그 수정

---
+++ frontend/src/components/login/AdminLogin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  </div>
+</template>
+
+
+<script setup>
+import {router} from "../../router/index.js";
+import {onMounted} from "vue";
+import {checkLoginStatus} from "../../store/auth.js";
+
+
+onMounted(async () => {
+  const loginSuccess = await checkLoginStatus();
+  if (loginSuccess) {
+    await router.push({ path: '/' });
+    location.reload();
+  } else {
+    alert('로그인 실패');
+    await router.push({path: '/login'})
+  }
+});
+
+</script>
