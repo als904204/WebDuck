d1363e64a793379ac8c9c23bfcb42e59fca1454a
Min <als904204@naver.com>
2024-04-19 21:43:33 +0900

    refactor[FE]배포 환경 출력

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,8 +9,12 @@ import Navbar from "./components/common/Navbar.vue";
 import Footer from "./components/common/Footer.vue";
 import {checkLoginStatus, isLoggedIn} from "./store/auth.js";
 import {onMounted} from "vue";
+const envUrl = import.meta.env.VITE_APP_BACKEND_URL;
+const envConfig = process.env.NODE_ENV;
 
 onMounted(async () => {
+  console.log(envUrl);
+  console.log(envConfig)
   isLoggedIn.value = sessionStorage.getItem('isLoggedIn') === 'true';
   // 새로고침 CSRF 토큰을 다시 요청
   if (isLoggedIn.value) {
